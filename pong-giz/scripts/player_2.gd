9ed83b134d673253177e8a29845e586f86bff34e
Fábio Sanzzoni <168327984+fabiosanzzoni@users.noreply.github.com>
2024-11-20 18:36:15 -0300

    The ball now have a initial direction in the game!
    The ball now have a initial direction in the game!

---
+++ pong-giz/scripts/player_2.gd
@@ -0,0 +1,12 @@
+extends CharacterBody2D
+
+var speed: int = 300
+
+func _physics_process(delta: float) -> void:
+	
+	position.y = clamp(position.y, 70, 575)
+	
+	if Input.is_action_pressed("keyboard_up"):
+		position.y -= speed * delta
+	if Input.is_action_pressed("keyboard_down"):
+		position.y += speed * delta
